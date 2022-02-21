98a5a0e5f26ab3a5ff2fa13346d44e82f2cac0b0
Stephanie Helm <stephaniemhelm@gmail.com>
2022-02-21 08:44:15 -0700

    tea service

--- app/services/tea_service.rb
+++ app/services/tea_service.rb
@@ -3,5 +3,13 @@ class TeaService
     conn = Faraday.new("https://tea-api-vic-lo.herokuapp.com")
     response = conn.get("/tea")
 
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_one_tea(name)
+    conn = Faraday.new("https://tea-api-vic-lo.herokuapp.com")
+    response = conn.get("/tea/#{name}")
+
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/tea_service_spec.rb
+++ spec/services/tea_service_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Tea API Service' do
   it 'can get all teas' do
     response = TeaService.get_all_teas
-    require "pry"; binding.pry
 
   end
 
+  it 'can get one tea by name' do
+    name = 'green'
+    response = TeaService.get_one_tea(name)
+    require "pry"; binding.pry
+  end
 end
