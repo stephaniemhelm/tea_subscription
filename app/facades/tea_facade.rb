23bf06e46b9df657991e00d067d79c5a99507986
Stephanie Helm <stephaniemhelm@gmail.com>
2022-02-21 10:10:35 -0700

    complete tea

--- app/facades/tea_facade.rb
+++ app/facades/tea_facade.rb
@@ -1,4 +1,12 @@
 class TeaFacade
+  def self.get_all_tea_data
+    teas = TeaService.get_all_teas
+
+    teas.map do |data|
+      Tea.new(data)
+    end 
+  end
+
   def self.get_tea_data(name)
     data = TeaService.get_one_tea(name)
 
--- spec/facades/tea_facade_spec.rb
+++ spec/facades/tea_facade_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe TeaFacade do
     expect(result.temperature).to eq(80)
     expect(result.brew_time).to eq(2)
   end
+
+  it 'can get data for all teas' do
+    result = TeaFacade.get_all_tea_data
+
+    green = result.first
+    black = result.second
+
+    expect(green.id).to eq("5fa3fd48d5ba620017ec1c09")
+    expect(green.title).to eq("green")
+    expect(green.description).to eq("Rich in antioxidants and reduces inflammation.")
+    expect(green.temperature).to eq(80)
+    expect(green.brew_time).to eq(2)
+
+    expect(black.id).to eq("5fa3fdb0d5ba620017ec1c0a")
+    expect(black.title).to eq("black")
+    expect(black.description).to eq("Boosts heart health and lowers cholesterol.")
+    expect(black.temperature).to eq(85)
+    expect(black.brew_time).to eq(3)
+  end
 end
