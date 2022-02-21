5ff1d34bfb44a412ea993a7163d8fcbd9053e364
Stephanie Helm <stephaniemhelm@gmail.com>
2022-02-21 09:55:22 -0700

    tea specs

--- spec/facades/tea_facade_spec.rb
+++ spec/facades/tea_facade_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe TeaFacade do
     name = 'green'
     result = TeaFacade.get_tea_data(name)
 
-    #require "pry"; binding.pry
+    expect(result.id).to eq("5fa3fd48d5ba620017ec1c09")
+    expect(result.title).to eq("green")
+    expect(result.description).to eq("Rich in antioxidants and reduces inflammation.")
+    expect(result.temperature).to eq(80)
+    expect(result.brew_time).to eq(2)
   end
 end
